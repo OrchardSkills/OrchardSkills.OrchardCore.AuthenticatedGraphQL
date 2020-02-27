bd0049d4f2ccc4a138bb0920f2ea94d88b83b610
Orchard Skills <Sales@OrchardSkills.com>
2020-02-27 08:56:16 -0700

    Initial commit of source code

---
+++ ClientApp/src/app/graphql.module.ts
@@ -0,0 +1,66 @@
+import {NgModule} from '@angular/core';
+import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
+import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import { ApolloLink } from 'apollo-link';
+import { setContext } from'apollo-link-context';
+import { HttpClientModule } from '@angular/common/http';
+
+
+export function provideApollo(httpLink: HttpLink) {
+  const basic = setContext((operation, context) => ({
+    headers: {
+      Accept: 'charset=utf-8'
+    }
+  }));
+  
+  // Get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  const auth = setContext((operation, context) => ({
+    headers: {
+      Authorization: `Bearer ${token}`
+    },
+  }));
+  const link = ApolloLink.from([basic, auth, httpLink.create({ uri })]);
+  const cache = new InMemoryCache();
+
+  return {
+    link,
+    cache
+  }
+}
+
+const uri = 'https://OrchardCMS.net/api/graphql'; // <-- add the URL of the GraphQL server here
+export function createApollo(httpLink: HttpLink) {
+  return {
+    link: httpLink.create({uri}),
+    cache: new InMemoryCache(),
+  };
+}
+
+/*
+@NgModule({
+  exports: [ApolloModule, HttpLinkModule],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: createApollo,
+      deps: [HttpLink],
+    },
+  ],
+})
+export class GraphQLModule {}
+*/
+@NgModule({
+  exports: [
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule
+  ],
+  providers: [{
+    provide: APOLLO_OPTIONS,
+    useFactory: provideApollo,
+    deps: [HttpLink]
+  }]
+})
+export class GraphQLModule {}
