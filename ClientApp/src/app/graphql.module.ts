c7facf845dcf163fe9d2b122fbef9c4f51778f37
Angular Advisors <AngularAdvisors@gmail.com>
2020-06-26 14:55:40 -0600

    Updated to Orchard Core RC2 and Angular 10

--- ClientApp/src/app/graphql.module.ts
+++ ClientApp/src/app/graphql.module.ts
@@ -1,49 +1,26 @@
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { ApolloModule, Apollo, APOLLO_OPTIONS } from 'apollo-angular';
-import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloLink } from 'apollo-link';
-import {setContext} from 'apollo-link-context';
+import {NgModule} from '@angular/core';
+import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
+import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
 import { environment } from 'src/environments/environment';
 
 const uri = environment.serverURL + '/api/graphql/';
 
-export function provideApollo(httpLink: HttpLink) {
-  const basic = setContext((operation, context) => ({
-    headers: {
-      Accept: 'charset=utf-8'
-    }
-  }));
-
-  // Get the authentication token from local storage if it exists
-  // const token = localStorage.getItem('token');
-  const token = localStorage.getItem('access_token')
-  const auth = setContext((operation, context) => ({
-    headers: {
-      Authorization: `Bearer ${token}`
-    },
-  }));
-
-  const link = ApolloLink.from([basic, auth, httpLink.create({ uri })]);
-  const cache = new InMemoryCache();
-
+export function createApollo(httpLink: HttpLink) {
   return {
-    link,
-    cache
-  }
+    link: httpLink.create({uri}),
+    cache: new InMemoryCache(),
+  };
 }
 
 @NgModule({
-  exports: [
-    HttpClientModule,
-    ApolloModule,
-    HttpLinkModule
+  exports: [ApolloModule, HttpLinkModule],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: createApollo,
+      deps: [HttpLink],
+    },
   ],
-  providers: [{
-    provide: APOLLO_OPTIONS,
-    useFactory: provideApollo,
-    deps: [HttpLink]
-  }]
 })
 export class GraphQLModule {}
