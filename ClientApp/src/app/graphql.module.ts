a92d19cf111093d2545cce1392ddef3d9068d87c
sales <sales@angularadvisors.com>
2020-08-13 07:33:56 -0600

    Updated apollo and updated packages

--- ClientApp/src/app/graphql.module.ts
+++ ClientApp/src/app/graphql.module.ts
@@ -1,12 +1,11 @@
 import {NgModule} from '@angular/core';
-import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
-import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
-import {InMemoryCache} from 'apollo-cache-inmemory';
+import {APOLLO_OPTIONS} from 'apollo-angular';
+import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
+import {HttpLink} from 'apollo-angular/http';
 import { environment } from 'src/environments/environment';
-
 const uri = environment.serverURL + '/api/graphql/';
 
-export function createApollo(httpLink: HttpLink) {
+export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({uri}),
     cache: new InMemoryCache(),
@@ -14,7 +13,6 @@ export function createApollo(httpLink: HttpLink) {
 }
 
 @NgModule({
-  exports: [ApolloModule, HttpLinkModule],
   providers: [
     {
       provide: APOLLO_OPTIONS,
